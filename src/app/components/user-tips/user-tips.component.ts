9ad66ab30bd998d1aa9aa17dffa178e02134ea89
Yun Shan <yunshan21st@qq.com>
2025-12-09 16:02:24 +0800

    feat(user-tips): 动态渲染示例模板链接并优化提示信息布局

--- src/app/components/user-tips/user-tips.component.ts
+++ src/app/components/user-tips/user-tips.component.ts
@@ -7,16 +7,22 @@ import { environment } from '../../../environments';
 @Component({
   selector: 'lab-user-tips',
   standalone: true,
-  imports: [
-    AccordionModule,
-    ButtonModule,
-    TranslatePipe,
-  ],
+  imports: [AccordionModule, ButtonModule, TranslatePipe],
   templateUrl: './user-tips.component.html',
   styleUrl: './user-tips.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserTipsComponent {
-
   protected readonly environment = environment;
+
+  protected readonly examples: { name: string; url: string }[] = [
+    {
+      name: '四号谷地毕业示例',
+      url: 'aef/flow?z=eJwtxqEKgEAQBNC.2TAg7N15YpmyyWA02cSqTBGDXy.ivfTEWAFkEzfUwYFioiP3rQuSj-0dKgowmfigfA0T69.ZdF50O7Qz7GZKL9NzFbY_&v=11',
+    },
+    {
+      name: '武陵毕业示例',
+      url: 'aef/list?o=jinlong_coupon***2&o=originium_ore*360**3&o=quartz_sand*0**3&o=iron_ore*90**3&o=xiranite_powder*2*3*3*xiranite_oven_1&o=power_proc_battery_4*2*3**power_sta_1&omt=0&loc=A&v=11',
+    },
+  ];
 }

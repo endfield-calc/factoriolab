cb7528abfda81e40c61faf3a13b444c99b147859
Yun Shan <yunshan21st@qq.com>
2025-12-15 10:26:56 +0800

    fix(test): 修复user-tips测试

--- src/app/components/user-tips/user-tips.component.spec.ts
+++ src/app/components/user-tips/user-tips.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { TestModule } from '~/tests';
+
 import { UserTipsComponent } from './user-tips.component';
 
 describe('UserTipsComponent', () => {
@@ -8,7 +10,7 @@ describe('UserTipsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserTipsComponent]
+      imports: [TestModule, UserTipsComponent]
     })
     .compileComponents();
 

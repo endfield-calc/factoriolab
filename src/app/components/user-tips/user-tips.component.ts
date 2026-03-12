1fb0a24eb36c468f1e4c47c888c360d808fbe3c8
Yun Shan <yunshan21st@qq.com>
2026-03-12 13:25:39 +0800

    feat(user-tips): 更新用户提示组件中的示例链接
    Signed-off-by: Yun Shan <yunshan21st@qq.com>

--- src/app/components/user-tips/user-tips.component.ts
+++ src/app/components/user-tips/user-tips.component.ts
@@ -26,11 +26,15 @@ export class UserTipsComponent {
   protected readonly examples: { name: string; url: string }[] = [
     {
       name: '四号谷地毕业示例',
+      url: 'aef/flow?z=eJw1yDEKgDAQBMDfXLEg5C5GbLYJCBaWVunEVrlGRHy9iLEbxpkLTDWJc0HqAhDFWWA9IjCIM8Da2gfU.p6hoa.fIL09ivNGfJnFmT5O4rxgdfeDQTZfmeWk6gNmmh4R&v=11',
+    },
+    {
+      name: '谷地理论最高调度券(超出需求)',
       url: 'aef/flow?z=eJwtxqEKgEAQBNC.2TAg7N15YpmyyWA02cSqTBGDXy.ivfTEWAFkEzfUwYFioiP3rQuSj-0dKgowmfigfA0T69.ZdF50O7Qz7GZKL9NzFbY_&v=11',
     },
     {
-      name: '武陵毕业示例',
-      url: 'aef/list?o=jinlong_coupon***2&o=originium_ore*360**3&o=quartz_sand*0**3&o=iron_ore*90**3&o=xiranite_powder*2*3*3*xiranite_oven_1&o=power_proc_battery_4*2*3**power_sta_1&omt=0&loc=A&v=11',
+      name: '旧版本武陵毕业示例(需谷地传输铁零件)',
+      url: 'aef/flow?z=eJxFzLsOQFAQBNC.2WIK7rWexRYkHoWOhOhES7YR3y-LRHdmJhmVakWUx6SyglMHMKks8DkYqEnF4StHFJ8mxEFie0Mq1YzI3JmHx4zhD68nJEXI7hl7K1pkIdvbcYqjXTcp6RLvb4p4IhA_&v=11',
     },
   ];
 }

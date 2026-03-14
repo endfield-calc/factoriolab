3531bdd0a51169a796221d22fd434636cba27eb0
Yun Shan <14977331+Yun-Shan@users.noreply.github.com>
2026-03-14 14:17:57 +0800

    feat(user-tip): 更新示例方案

--- src/app/components/user-tips/user-tips.component.ts
+++ src/app/components/user-tips/user-tips.component.ts
@@ -33,12 +33,12 @@ export class UserTipsComponent {
       url: 'aef/flow?o=tundra_coupon***2&o=originium_ore*560**3&o=quartz_sand*240**3&o=iron_ore*1080**3&o=power_proc_battery_3*4*3**power_sta_1&o=power_proc_battery_2**3&omt=0&loc=B&v=11',
     },
     {
-      name: '武陵毕业示例(暂定)(需谷地传输蓝铁块)',
-      url: 'aef/flow?z=eJxNjbEKAjEQRP9miyk0m-QOLbYwweMELSSKYnfYWKjbqN8vGyLYvZkd3qqkCT07UklPIMBjTyoT4sIBgVQu8BEBWJOKQysPWDY6Ic46uw-kUuA7gE12A.s2yTv0tjiSCrcaxUYFVW2cR7Cr.7eWhv-U3vBzM6UErpA35sBo3fmHj5c4uutVVvQR5i-dgTMN&v=11',
+      name: '武陵毕业示例(需谷地传输致密源石粉末)',
+      url: 'aef/flow?z=eJxFzLEKAjEQhOG3SfGDupvLHVpsYQ5EC6sTBLvDxkJJoz6.ZI1YzccwTLE8M6iEYjNpLdCFYhdicgitObFpOpOWvSvf0Nja8cjg0NYx1cVE-h32oHW4R4WOWL37O7-IK3.NqGM8fK8XHo-nSbiXq23D21Q.U1UrIA__&v=11',
     },
     {
-      name: '旧版本武陵毕业示例(需谷地传输铁零件)',
-      url: 'aef/flow?z=eJxFzLsOQFAQBNC.2WIK7rWexRYkHoWOhOhES7YR3y-LRHdmJhmVakWUx6SyglMHMKks8DkYqEnF4StHFJ8mxEFie0Mq1YzI3JmHx4zhD68nJEXI7hl7K1pkIdvbcYqjXTcp6RLvb4p4IhA_&v=11',
+      name: '武陵理论最高调度券(超出需求)',
+      url: 'aef/flow?z=eJzLt3VK1NLSMlLLt03UMrEw0NIyVsu3jdIyMgEzDLSgIiFallBWuJaJnimY5ZShZWgEFXX21TIDMwyhYlrBIBXBWhBznD0hyizBVG6JrYFaTn6yraNama2hIQDwqh8.&v=11',
     },
   ];
 }

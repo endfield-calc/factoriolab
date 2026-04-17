e1d1ffa65f583061823e7235307acf3b887ab51a
Yun Shan <yunshan21st@qq.com>
2026-04-17 14:27:50 +0800

    docs(user-tips): 当前武陵毕业示例增加旧版本标识，暂时移除武陵理论最高调度券
    Signed-off-by: Yun Shan <yunshan21st@qq.com>

--- src/app/components/user-tips/user-tips.component.ts
+++ src/app/components/user-tips/user-tips.component.ts
@@ -33,12 +33,12 @@ export class UserTipsComponent {
       url: 'aef/flow?o=tundra_coupon***2&o=originium_ore*560**3&o=quartz_sand*240**3&o=iron_ore*1080**3&o=power_proc_battery_3*4*3**power_sta_1&o=power_proc_battery_2**3&omt=0&loc=B&v=11',
     },
     {
-      name: '武陵毕业示例(需谷地传输致密源石粉末)',
+      name: '武陵毕业示例(旧版本，需谷地传输致密源石粉末)',
       url: 'aef/flow?z=eJxFzLEKAjEQhOG3SfGDupvLHVpsYQ5EC6sTBLvDxkJJoz6.ZI1YzccwTLE8M6iEYjNpLdCFYhdicgitObFpOpOWvSvf0Nja8cjg0NYx1cVE-h32oHW4R4WOWL37O7-IK3.NqGM8fK8XHo-nSbiXq23D21Q.U1UrIA__&v=11',
     },
-    {
-      name: '武陵理论最高调度券(超出需求)',
-      url: 'aef/flow?z=eJzLt3VK1NLSMlLLt03UMrEw0NIyVsu3jdIyMgEzDLSgIiFallBWuJaJnimY5ZShZWgEFXX21TIDMwyhYlrBIBXBWhBznD0hyizBVG6JrYFaTn6yraNama2hIQDwqh8.&v=11',
-    },
+    // {
+    //   name: '武陵理论最高调度券(超出需求)',
+    //   url: 'aef/flow?z=eJzLt3VK1NLSMlLLt03UMrEw0NIyVsu3jdIyMgEzDLSgIiFallBWuJaJnimY5ZShZWgEFXX21TIDMwyhYlrBIBXBWhBznD0hyizBVG6JrYFaTn6yraNama2hIQDwqh8.&v=11',
+    // },
   ];
 }
